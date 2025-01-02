acafbf7583d6408353e5ab9587ac1defda873319
shreysamal <shreysamal101@gmail.com>
2025-01-02 23:09:05 +0530

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Home />
+    </div>
+     
+    
+    
+  )
+}
+
+export default App
